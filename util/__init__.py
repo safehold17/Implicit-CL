74a59d655a6f543f2aa36191d19ce2845aa7d4a1
chen <chen@chen.com>
2026-03-11 11:02:20 +0100

    fix indentation

--- util/__init__.py
+++ util/__init__.py
@@ -83,7 +83,7 @@ def cleanup_log_dir(log_dir, pattern='*'):
 
 def str2bool(v):
     if isinstance(v, bool):
-       return v
+        return v
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
     elif v.lower() in ('no', 'false', 'f', 'n', '0'):
